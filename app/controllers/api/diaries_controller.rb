59b1e3ad140d286a250a00e0f11b7813b6a3bc75
liangma87 <mliang3176@gmail.com>
2020-02-25 06:56:13 +0000

    fixes for diaries_controller

--- app/controllers/api/diaries_controller.rb
+++ app/controllers/api/diaries_controller.rb
@@ -6,7 +6,7 @@ module Api
       end
 
       def show
-        stock = Company.find_by_symbol(params[:ticker])
+        stock = Company.find(params[:id])
         diaries = stock.diaries
         render json: diaries
       end
