c098f9d38dffffd81a9f102847ef241860bbfec4
liangma87 <mliang3176@gmail.com>
2020-03-05 05:39:36 +0000

    change the diaries query using created_at field

--- app/controllers/api/diaries_controller.rb
+++ app/controllers/api/diaries_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class DiariesController < ApplicationController
       def index
-        diaries = Diary.all.order(updated_at: :DESC)
+        diaries = Diary.all.order(created_at: :DESC)
         render json: diaries
       end
 
